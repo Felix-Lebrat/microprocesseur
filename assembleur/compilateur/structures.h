45d8f88da96c6025750281663d5f2521853253a0
Florian Tousnakhoff <florian.tousnakhoff@gmail.com>
2021-11-19 13:56:55 +0100

    Début du compilateur d'assembleur ( juste le lexing et parsing )

---
+++ assembleur/compilateur/structures.h
@@ -0,0 +1,33 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+struct Reg
+{	
+	std::string name;
+	bool code[16];		// Un registre est représenté par le code qui lui correspond
+};
+
+struct Op
+{
+	std::string name;
+	bool code[16];
+}
+
+struct Program  // 
+{
+	std::vector
+};
+
+/*
+ *	Un code assembleur est constitué de labels (q
+ *
+ *
+ *
+ *
+ */
+
+#endif

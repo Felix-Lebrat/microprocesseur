c969ddbecda69f1d16b2dd9e30743973e410e162
florian <florian.tousnakhoff@gmail.com>
2022-01-24 23:36:46 +0100

    OK

--- horloge.c
+++ horloge.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
+	system("make");
 	if (argc > 1){
 		if(argv[1][0] == '-' && argv[1][1] == 'f'){
-			system("cp assembleur/pssembleur/clock/clock_speed.pss.rom fe_ROM0");
+			system("cp clock_speed.pss.rom fe_ROM0");
 		}
 		else{
 			printf("Argument invalide : il faut faire -f pour le mode rapide");
 		}
 	}
 	else if (argc == 1){
-			system("cp assembleur/pssembleur/clock/clock.pss.rom fe_ROM0");
+			system("cp clock.pss.rom fe_ROM0");
 	}
 	
 	system("assembleur/pssembleur/clock/microprocesseur -a");
